5498e729d8028e75c1a556e1f3e1796a1576568e
John <joonleno122213@gmail.com>
2024-10-29 22:07:43 -0300

    criação dos bancos User e Transaction

---
+++ prisma/migrations/20241030010658_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "endereco" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "data" DATETIME NOT NULL,
+    "remetente" TEXT NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "valor" REAL NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
