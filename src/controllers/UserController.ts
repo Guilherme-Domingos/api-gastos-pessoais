936c0b98d03af2b0cade11345b5bffe29a22b053
John <joonleno122213@gmail.com>
2024-11-03 14:13:36 -0300

    CRUD

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -21,4 +21,22 @@ export class UserController {
             res.status(500).json({ message: `Erro ao registrar usuário: ${error}` });
         }
     }
+
+    public async updateUser(req: Request, res: Response): Promise<void> {
+        try{
+            const id = req.params.id;
+            const updatedUser = await userDao.updateUser(id, req.body);
+            res.status(200).json(updatedUser);
+        }catch(error){
+            res.status(500).json({ message: `Erro ao atualizar usuário: ${error}`})
+        }
+    }
+
+    public async deleteUser(req: Request, res: Response): Promise<void> {
+        try {
+            const user = await userDao.deleteUser(req.body);
+        }catch (error) {
+            res.status(500).json({ message: `Erro ao deletar usuário: ${error}` });
+        }
+    }
 }
--- src/dao/UserDao.ts
+++ src/dao/UserDao.ts
@@ -28,4 +28,37 @@ export class UserDao {
             throw new Error(`Erro ao registrar usuário: ${error}`);
         }
     }
+
+    public async updateUser(id: string, data: { nome: string; email: string; telefone: string; endereco: string; senha: string }) {
+        try {
+            const user = await prisma.user.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    nome: data.nome,
+                    email: data.email,
+                    telefone: data.telefone,
+                    endereco: data.endereco,
+                    senha: data.senha,
+                },
+            });
+            return user;
+        }catch (error) {
+            throw new Error(`Erro ao atualizar usuário: ${error}`);
+        }
+    }
+
+    public async deleteUser( id: string) {
+        try{
+            const user = await prisma.user.delete({
+                where: {
+                    id: id,
+                    }
+                });
+        }catch (error) {
+            throw new Error(`Erro ao deletar usuário: ${error}`);
+        }
+    }
+
 }
