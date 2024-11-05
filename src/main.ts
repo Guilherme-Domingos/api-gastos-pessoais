2ed508c784ee52ad6efcf254b42999746dd17b16
JohnLenon22 <joonleno122213@gmail.com>
2024-11-05 16:07:23 -0300

    main atualizado

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
-import express from 'express';
+import {Api} from '../src/api/api';
 import {UserController} from "../src/controllers/UserController"
+import { TransactionController } from './controllers/TransactionController';
 
-const app = express();
-app.use(express.json());
 const userController = new UserController();
+const  transactionController = new TransactionController();
 
 
-app.get('/users', (req, res) => 
-    userController.getAllUsers(req, res)
-);
+function main(){
+    const api = Api.build()
 
-app.post('/registerUser', (req, res) =>
-    userController.registerUser(req, res)
-);
+    api.addRota('/users', 'GET', userController.getAllUsers);
+    api.addRota('/registerUser', 'POST', userController.registerUser);
+    api.addRota('/updateUser','PUT', userController.updateUser)
+    api.addRota('/deleteUser','DELETE', userController.deleteUser)
 
+    api.addRota('transaction', 'GET', transactionController.getTransactions)
+    // api.addRota('/transaction', 'POST', transactionController.createTransaction)
+    // api.addRota('/transaction', 'PUT', transactionController.updateTransaction)
+    // api.addRota('/transaction', 'DELETE', transactionController.deleteTransaction)
+    api.start()
+}
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+main()
