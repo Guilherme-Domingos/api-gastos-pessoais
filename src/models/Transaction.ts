faa82e85f9182c8be776352a4f7c5eb5d17db9f1
John <joonleno122213@gmail.com>
2024-11-18 22:46:36 -0300

    alteracoes no models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,15 +1,15 @@
 export class Transacao{
     public readonly id: string;
-    public readonly data: Date;
+    public readonly date: Date;
     public readonly rementente: string;
     public readonly categoria:  string;
     public readonly valor:  number;
     public readonly descricao: string;
     public readonly tipo: string;
 
-    constructor(data: Date, rementente: string,  categoria: string, valor: number, descricao: string, tipo: string) {
+    constructor(date: Date, rementente: string,  categoria: string, valor: number, descricao: string, tipo: string) {
         this.id = crypto.randomUUID();
-        this.data = data;
+        this.date = date;
         this.rementente = rementente;
         this.categoria = categoria;
         this.valor = valor;
