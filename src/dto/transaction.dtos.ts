f617062ebf0d42a1a74a14decb4a673fcbd032bf
JohnLenon22 <joonleno122213@gmail.com>
2024-11-26 16:53:24 -0300

    implementacao do dto

--- src/dto/transaction.dtos.ts
+++ src/dto/transaction.dtos.ts
@@ -1,50 +1,38 @@
 import { IsString, IsDateString, IsOptional, IsNumber, IsEnum } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
 
-export class RegisterDtoTransaction {
-  @IsDateString()
-  date: string;  // Caso seja uma string no formato de data (ISO 8601)
+export class registerDtoTransaction {
+    @IsDateString()
+    date: string; 
 
-  @IsString()
-  remetente: string;
-
-  @IsString()
-  categoria: string;
-
-  @IsNumber()
-  valor: number;
-
-  @IsOptional()  
-  @IsString()
-  descricao?: string;
-
-  @IsEnum(['Receita', 'Despesa'])  
-  tipo: 'Receita' | 'Despesa';
-
-  @IsString()
-  userId: string;
-
-  // Construtor: agora tipo e descricao são tratados corretamente
-  constructor(remetente: string, categoria: string, valor: number, tipo: 'Receita' | 'Despesa', userId: string, date?: string, descricao?: string
-  ) {
-    this.date = date || '';  
-    this.remetente = remetente;
-    this.categoria = categoria;
-    this.valor = valor;
-    this.tipo = tipo;  
-    this.userId = userId;
-    this.descricao = descricao;  
-  }
-}
-
-//////OUU
-
-export interface RegisterDtoTransaction {
-    date: string;  
+    @IsString()
     remetente: string;
+
+    @IsString()
     categoria: string;
+
+    @IsNumber()
     valor: number;
+
+    @IsOptional()  
+    @IsString()
     descricao?: string;
+
+    @IsEnum(['Receita', 'Despesa'])  
     tipo: 'Receita' | 'Despesa';
+
+    @IsString()
     userId: string;
+
+    // Construtor: agora tipo e descricao são tratados corretamente
+    constructor(remetente: string, categoria: string, valor: number, tipo: 'Receita' | 'Despesa', userId: string, date?: string, descricao?: string){
+        this.date = date || '';  
+        this.remetente = remetente;
+        this.categoria = categoria;
+        this.valor = valor;
+        this.tipo = tipo;  
+        this.userId = userId;
+        this.descricao = descricao;  
+    }
 }
+
